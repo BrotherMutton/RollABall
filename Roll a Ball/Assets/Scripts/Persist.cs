a0af99fc6b053b0b748544cafd7ee901c6f655fd
BrotherMutton <matthew.wisner@knights.ucf.edu>
2019-07-04 18:55:11 -0400

    Challenge 1 complete

---
+++ Roll a Ball/Assets/Scripts/Persist.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persist : MonoBehaviour
+{
+    public bool Persistent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Persistent == true)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        if (Persistent == false)
+        {
+
+        }
+        
+    }
+
+}
--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,18 +11,28 @@ public class PlayerController : MonoBehaviour
 
     public Text countText;
     public Text winText;
+    public Text scoreText;
+    public Text livesText;
+
+    public GameObject playerObject;
 
     private Rigidbody rb;
-    private int count;
+    static int count=0;
+    static int score=0;
+    static int lives=3;
+    private Vector3 origin;
 
 
-    void Start ()
+
+    void Awake ()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
+        origin = new Vector3(0,0.5f,0);
+
         SetCountText ();
         winText.text = "";
-        
+        DontDestroyOnLoad(playerObject);     
+
     }
     void FixedUpdate ()
     {
@@ -36,20 +47,42 @@ public class PlayerController : MonoBehaviour
              Application.Quit();
     }
 
+    // Pick up and Mine functionality
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
             {
                 other.gameObject.SetActive (false);
                 count = count + 1;
+                score = score + 5;
                 SetCountText ();
             }
+        if (other.gameObject.CompareTag("Mine"))
+        {
+            other.gameObject.SetActive (false);
+            score = score - 5;
+            lives = lives - 1;
+            SetCountText ();
+        }
     }
-
+    // Increment of Score and Lives also occurs here
     void SetCountText ()
     {
+        livesText.text = "Lives: " + lives.ToString ();
         countText.text = "Count: " + count.ToString ();
-        if (count >= 10)
+        scoreText.text = "Score: " + score.ToString ();
+        if (lives == 0)
+        {
+            winText.text = "You lost! :(";
+            playerObject.SetActive (false);
+            
+        }
+         if (count == 10)
+        {
+            transform.position = origin;
+            SceneManager.LoadScene("Level2");
+        }
+        if (score >= 90)
         {
             winText.text = "You Win!";
         }
